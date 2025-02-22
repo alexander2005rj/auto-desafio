46e647059c2b6ab88d04e4999f56136d449919f9
Alexander Ribeiro <alexander2005rj@gmail.com>
2025-02-22 11:01:51 -0300

    feat: add relatorio.

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,23 @@
 const { defineConfig } = require("cypress");
+const { beforeRunHook, afterRunHook } = require('cypress-mochawesome-reporter/lib');
 
 module.exports = defineConfig({
+  requestTimeout: 5000,
+  responseTimeout: 5000,
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    charts: true,
+    embeddedScreenshots: true,
+    inlineAssets: false,
+    videoOnFailOnly: true,
+    saveAllAttempts: false,
+    overwrite: true
+  },
   e2e: {
     baseUrl: "https://autoforce-academy.pilotodetestes.com.br",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on)
     },
   },
 });
